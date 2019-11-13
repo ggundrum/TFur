b9a02735620acb9704003e5768a023c77390ee0f
Greyson Gundrum <ggundrum@Greysons-MacBook-Pro.local>
2019-11-13 15:48:47 -0700

    conclusing of chapter 2 web cluster up and running.

--- main.tf
+++ main.tf
@@ -2,22 +2,6 @@ provider "aws" {
  region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-
-  tags = {
-    Name = "terraform-example"
-  }
-}
-
 resource "aws_launch_configuration" "example" {
   image_id                    = "ami-0c55b159cbfafe1f0"
   instance_type               = "t2.micro"
@@ -35,7 +19,10 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
     launch_configuration = aws_launch_configuration.example.name
-    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+    vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+    target_group_arns    = [aws_lb_target_group.asg.arn]
+    health_check_type    = "ELB"
 
     min_size = 2
     max_size = 10
@@ -63,9 +50,9 @@ variable "server_port" {
     default = "8080"
 }
 
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "public IP address of the web server"
+output "alb_dns_name" {
+    value = aws_lb.example.dns_name
+    description = "The domain name of the load balancer"
 }
 
 data "aws_vpc" "default" {
@@ -79,11 +66,12 @@ data "aws_subnet_ids" "default" {
 resource "aws_lb" "example" {
   name               = "terraform-asg-example"
   load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
   subnets            = data.aws_subnet_ids.default.ids
 }
 
-resource "aws_alb_listener" "http" {
-  load_balancer_arn = "aws_lb.example.arn"
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
   port              = 80
   protocol          = "HTTP"
 
@@ -95,5 +83,53 @@ resource "aws_alb_listener" "http" {
       message_body = "404: page not found"
       status_code  = 404
     }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol             = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource aws_alb_listener_rule "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    field  = "path-pattern"
+    values = ["*"]
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
