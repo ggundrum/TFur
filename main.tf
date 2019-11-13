ac5ce86315f6a7c08b03ee51b671ac60c553464b
Greyson Gundrum <ggundrum@Greysons-MacBook-Pro.local>
2019-11-13 14:35:23 -0700

    It's broken. going to ctrl+z and see where.

--- main.tf
+++ main.tf
@@ -2,10 +2,63 @@ provider "aws" {
  region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-    ami  = "ami-40d28157"
-    instance_type = "t2.micro"
-    tags = {
-        name = "terraform-example"
+resource "aws_launch_configuration" "example" {
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+  lifecycle {
+      create_before_destroy = true
+  }
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration = aws_launch_cofiguration.example.name
+    vpc_zone_identifier = data.aws_subnet_ids.example.id
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                 = "Name"
+        value               = "terraform-asg-example"
+        propagate_at_launch = true
     }
+}
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+    default = "8080"
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+    description = "public IP address of the web server"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = aws_vpc.default.id
 }
